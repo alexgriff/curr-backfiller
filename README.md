e6072e585651428fa2e0fb66c70ffbf10db01b82
Alex Griffith <alexalexgriffith@gmail.com>
2019-07-24 15:25:56 -0400

    Add auth

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # INSTRUCTIONS
 
-Run `python index.py` from the root of this project
+Run `python index.py` from the root of this project. The assumption is there is a folder of repositories to be updated that lives elsewhere
 
 edit the variables at the top of the file
 
 ```
 # CHANGE THESE
-owner="alexgriff"
-path_to_labs = os.path.join(os.path.realpath(".."), "labs")
+owner="alexgriff" # github org or username
+path_to_labs = os.path.join(os.path.realpath(".."), "labs") # path to lesson repos
+oauth_token = os.environ['OAUTH_TOKEN'] # github oauth token value
 ```
 Here, for example, there is a directory called `labs` one level above this project.
 
 The `labs` directory contains local repos where the remote live at `github.com/alexgriff/<name-of-repo>`
 
+In this example, there is an environment variable called `OAUTH_TOKEN` that holds a [GitHub Oauth token](https://help.github.com/en/articles/git-automation-with-oauth-tokens). Feel free to paste a string here directly as long as it's not pushed to GitHub.
+
 
 ## TODO
-* configure Authentication Headers & SSH so can work for learn-co-curriculm org repos that may be private. Won't rate limit API, etc.
-* make it 'do nothing' on a repo without a solution branch (ie not a lab)
+* configure ~Authentication Headers~ & ~SSH~(i think????) so can work for learn-co-curriculm org repos that may be private. Won't rate limit API, etc.
+* ~make it 'do nothing' on a repo without a solution branch (ie not a lab)~
--- index.py
+++ index.py
@@ -3,34 +3,39 @@ import base64
 import json
 import os
 import subprocess
-from git import Repo, GitCommandError
+from git import Repo, Git, GitCommandError
 
 # CONSTANTS
 SOLUTION_TAG = "__SOLUTION__"
 CURRICULUM_BRANCH = "curriculum-team"
 
 # CHANGE THESE
-owner="alexgriff"
-path_to_labs = os.path.join(os.path.realpath(".."), "labs")
+owner="alexgriff" # github org or username
+path_to_labs = os.path.join(os.path.realpath(".."), "labs") # path to lesson repos
+oauth_token = os.environ['OAUTH_TOKEN'] # github oauth token value
 
 # FUNCTIONS
 def create_merged_notebook(lab):
     master_content = get_notebook_contents(lab)
     sol_content = get_notebook_contents(lab, branch="solution")
 
-    cells = merge_cells(master_cells = master_content["cells"], sol_cells = sol_content["cells"])
+    if sol_content:
+        cells = merge_cells(master_cells = master_content["cells"], sol_cells = sol_content["cells"])
 
-    master_content.update({"cells": cells})
-
-    return json.dumps(master_content)
-    # f = open(f"{lab}.ipynb", "w")
-    # f.write(json.dumps(master_content))
-    # f.close()
+        master_content.update({"cells": cells})
+        return json.dumps(master_content)
+    else:
+        return None
 
 def get_notebook_contents(lab, branch="master"):
-    response = requests.get(f"http://api.github.com/repos/{owner}/{lab}/contents/index.ipynb?ref={branch}")
-    encoded_content = json.loads(response.content)['content']
-    return json.loads(base64.b64decode(encoded_content))
+    headers = {"Authorization": f"token {oauth_token}"}
+    response = requests.get(f"http://api.github.com/repos/{owner}/{lab}/contents/index.ipynb?ref={branch}", headers=headers)
+
+    if response.status_code == 200:
+        encoded_content = json.loads(response.content)['content']
+        return json.loads(base64.b64decode(encoded_content))
+    else:
+        return None
 
 
 def tag_cell(cell):
@@ -56,6 +61,10 @@ def merge_cells(master_cells = [], sol_cells = []):
 
 # RUN
 # ============================
+git_ssh_identity_file = os.path.expanduser('~/.ssh/id_rsa')
+git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
+
+Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd)
 
 labs = os.listdir(path_to_labs)
 
@@ -63,39 +72,42 @@ for lab in labs:
     # create new json
     merged_nb_json = create_merged_notebook(lab)
 
-    # cd into repo
-    os.chdir(f"{path_to_labs}/{lab}")
-    cwd = os.getcwd()
-    repo = Repo(cwd)
-    git = repo.git
+    if merged_nb_json:
+        # cd into repo
+        os.chdir(f"{path_to_labs}/{lab}")
+        cwd = os.getcwd()
+        repo = Repo(cwd)
+        git = repo.git
 
-    # switch to curriculum branch if exists or create new branch
-    try:
-        git.checkout(CURRICULUM_BRANCH)
-    except GitCommandError:
-        git.checkout("HEAD", b=CURRICULUM_BRANCH)
+        # switch to curriculum branch if exists or create new branch
+        try:
+            git.checkout(CURRICULUM_BRANCH)
+        except GitCommandError:
+            git.checkout("HEAD", b=CURRICULUM_BRANCH)
 
-    # write index.ipynb
-    f = open(f"{cwd}/index.ipynb", "w")
-    f.write(merged_nb_json)
-    f.close()
+        # write index.ipynb
+        f = open(f"{cwd}/index.ipynb", "w")
+        f.write(merged_nb_json)
+        f.close()
 
-    # generate markdown
-    subprocess.call(["jupyter", "nbconvert", "index.ipynb",  "--to", "markdown"])
-    subprocess.call(["mv", "index.md", "README.md"])
+        # generate markdown
+        subprocess.call(["jupyter", "nbconvert", "index.ipynb",  "--to", "markdown"])
+        subprocess.call(["mv", "index.md", "README.md"])
 
 
-    # add, commit, push
-    git.add(".")
-    try:
-        git.commit("-m", "AUTO: Create curriculum-team branch")
-        print(f"Added Commit: {repo.commit()}")
-    except GitCommandError:
-        print("Nothing to commit")
+        # add, commit, push
+        git.add(".")
+        try:
+            git.commit("-m", "AUTO: Create curriculum-team branch")
+            print(f"Added Commit: {repo.commit()}")
+        except GitCommandError:
+            print("Nothing to commit")
 
-    print(f"pushing to remote {CURRICULUM_BRANCH} branch for {lab}")
-    repo.git.push("origin", CURRICULUM_BRANCH)
+        print(f"pushing to remote {CURRICULUM_BRANCH} branch for {lab}")
+        repo.git.push("origin", CURRICULUM_BRANCH)
 
-    # clean up
-    git.checkout("master")
-    os.chdir(os.getcwd())
+        # clean up
+        git.checkout("master")
+        os.chdir(os.getcwd())
+    else:
+        print("Skipping, No solution branch")
